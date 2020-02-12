6c55d4336c4aea33e855a61fd0634cdb7eb1c801
nathalie-frenette <60408100+nathalie-frenette@users.noreply.github.com>
2020-02-12 12:39:18 -0500

    Updated Game (Video demo version)
    Jump script updated so its less glitchy
    I included all the necessary game objects in the prefabs folder.

---
+++ Scripts/quit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    private float screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = 0 - Camera.main.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < screenBounds * 3)
+        {
+            Debug.Log("Out");
+            UnityEditor.EditorApplication.isPlaying = false;
+            //Application.Quit();
+        }
+    }
+}
