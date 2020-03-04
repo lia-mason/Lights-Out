35a82ecf917d6f1de30dc5fbff6a2e011be18bc9
emilyzhaoo <47041119+emilyzhaoo@users.noreply.github.com>
2020-03-04 14:18:08 -0500

    Add files via upload
    for the start menu and scene changes!

---
+++ LoadAfterTime.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class LoadAfterTime : MonoBehaviour
+{
+    [SerializeField]
+    private float loadingDelay = 10f;
+    [SerializeField]
+    private string loadScene;
+    private float timeElapsed; 
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime; 
+        if (timeElapsed > loadingDelay)
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene(loadScene); 
+        }
+    }
+}
---
+++ LoadScene.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField]
+    private string scene;
+    public void NextScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
